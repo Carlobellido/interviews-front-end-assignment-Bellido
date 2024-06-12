ffdbe0358206a31b0fbc108dc8002bb8119adca2
Carlobellido <carlo_del_piero@hotmail.com>
2024-06-12 21:18:56 +0200

    Creazione progetto, impostazione architettura (components, page) e import React Router

---
+++ Recipe Book/src/main.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import RecipePage from './routes/RecipePage'
+import Errorpage from './routes/Errorpage'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RecipePage />,
+    errorElement: <Errorpage />,
+    
+
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+       <RouterProvider router={router} />
+  </React.StrictMode>,
+);
---
+++ Recipe Book/src/routes/RecipePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function RecipePage() {
+  return (
+    <div>
+      <h1>Las Recetas!!</h1>
+    </div>
+  )
+}
+
+export default RecipePage
